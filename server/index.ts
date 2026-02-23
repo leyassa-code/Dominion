40a2c7fc143f1c14173dd011121de5d4dc00b899
leyassa-code <leyassakingdavid@gmail.com>
2026-02-23 10:33:16 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -90,8 +90,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.app.listen(PORT,"0.0.0.0", () = {console.log('server running on port ${PORT}');
-    {
+  httpServer.listen(
       port,
       host: "0.0.0.0",
       reusePort: true,
